39a790b04f842c025dde5180aeac7aded010886a
Wille2002 <112902334+Wille2002@users.noreply.github.com>
2024-09-25 15:59:49 +0200

    PacMan Start

---
+++ index.js
@@ -0,0 +1,117 @@
+let gameDiv = document.createElement("div");
+let body = document.querySelector("body");
+body.appendChild(gameDiv);
+
+
+gameDiv.setAttribute("id", "container");
+
+
+
+
+let map_layout = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+let numbOfColums = map_layout[0].length;
+
+let row = 2;
+let column = 2;
+
+
+
+
+
+/*
+let gameDivDimentions = gameDiv.getBoundingClientRect();
+
+let tileWidth = (gameDivDimentions.width/map_layout[0].length);
+let tileHeight = (gameDivDimentions.height/map_layout.length);
+*/
+let counter = 0;
+map_layout.forEach(function(row, rowIndex){
+    row.forEach(function(cell, cellIndex) {
+        let div = document.createElement("div");
+        div.classList.add(counter);
+        if (cell == 0) {
+            div.classList.add("path");
+        }else if(cell == 1){
+            div.classList.add("edge");
+        }
+        counter++;
+        gameDiv.appendChild(div)
+    });
+});
+
+
+
+document.addEventListener("keydown", function(event){
+    event.preventDefault();
+    let index = (row) * numbOfColums + (column);
+   
+    switch (event.key.toLocaleLowerCase()) {
+        case "w":
+            row=row-1;
+            break;
+        case "a":
+            column=column-1;
+            break;
+        case "s":
+            row++;
+            break;
+        case "d":
+            column++; 
+            break;
+        default:
+            break;
+        }
+
+
+        // Något knasigt med dessa if satser FIXA!!
+        if (row>=0 && row< map_layout.length && column >= 0 && column < numbOfColums) {
+            if (map_layout[row][column] == 0) {     
+                let previousIndex = index;
+                index = (row) * numbOfColums + (column);
+                UpdatePreviousPosition(previousIndex)
+                UpdatePlayerPosition(index, previousIndex)
+                
+            }
+        }
+
+    })
+    
+    
+    let playerDiv;
+    function UpdatePlayerPosition(position, previousPosition) {
+        playerDiv = gameDiv.childNodes[position];
+        playerDiv.style.backgroundColor = "yellow";
+        
+}
+
+    function UpdatePreviousPosition(position) {
+        playerDiv = gameDiv.childNodes[position];
+        playerDiv.style.backgroundColor = "black";
+        
+    
+    }
+
